671a2e8d76209bec9a0a199ec21c721c14151e11
dh0522 <gong4857@naver.com>
2024-05-28 19:07:24 +0900

    회원service 개발

---
+++ src/main/java/jpabook2/jpashop2/service/MemberService.java
@@ -0,0 +1,73 @@
+package jpabook2.jpashop2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook2.jpashop2.domain.Member;
+import jpabook2.jpashop2.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+// 데이터 변경하는 것에서는 꼭 Tranactional이 있어야함.
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+
+	private MemberRepository memberRepository;
+
+
+	// 회원 가입
+	@Transactional
+	public Long join( Member member ){
+
+		// 중복회원 검증
+		validateDuplicateMember(member);
+
+		memberRepository.save(member);
+		return member.getId();
+
+	}
+
+	private void validateDuplicateMember(Member member) {
+		// exception
+
+		List<Member> findMembers = memberRepository.findByName(member.getName());
+		if( !findMembers.isEmpty() ){
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		}
+
+	}
+
+	// 회원 전체 조회
+
+	public List<Member> findMembers(){
+		return memberRepository.findAll();
+	}
+
+	public Member findOne(Long memberId){
+		return memberRepository.findOne(memberId);
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
