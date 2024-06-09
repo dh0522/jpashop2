de3e2795bb08efe7d01523c5193da7fd5330f5c0
dh0522 <gong4857@naver.com>
2024-06-09 14:58:02 +0900

    상품 수정 & html 수정

--- src/main/java/jpabook2/jpashop2/controller/ItemController.java
+++ src/main/java/jpabook2/jpashop2/controller/ItemController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jpabook2.jpashop2.domain.item.Book;
@@ -53,7 +55,40 @@ public class ItemController {
 		return "items/itemList";
 	}
 
-	
+	@GetMapping("/items/{itemId}/edit")
+	public String updateItemForm(@PathVariable("itemId") Long itemId,Model model){
+		Book item = (Book) itemService.findOne(itemId);
+
+		BookForm form = new BookForm();
+
+		form.setId(item.getId());
+		form.setName(item.getName());
+		form.setPrice(item.getPrice());
+		form.setStockQuantity(item.getStockQuantity());
+		form.setAuthor(item.getAuthor());
+		form.setIsbn(item.getIsbn());
+
+		model.addAttribute("form",form);
+
+		return "items/updateItemForm";
+	}
+
+	@PostMapping("/items/{itemId}/edit")
+	public String updateItem( @PathVariable("itemId") Long itemId, @ModelAttribute("form") BookForm form){
+
+		Book book = new Book();
+
+		book.setId(form.getId());
+		book.setName(form.getName());
+		book.setPrice(form.getPrice());
+		book.setAuthor(form.getAuthor());
+		book.setStockQuantity(form.getStockQuantity());
+		book.setIsbn(form.getIsbn());
+
+		itemService.saveItem(book);
+
+		return "redirect:/items";
+	}
 }
 
 
