2e34c265b89007786fe16b4d45f89f362cdba5f4
dh0522 <gong4857@naver.com>
2024-06-09 16:42:20 +0900

    변경 감지와 병합

--- src/main/java/jpabook2/jpashop2/service/ItemService.java
+++ src/main/java/jpabook2/jpashop2/service/ItemService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jpabook2.jpashop2.domain.item.Book;
 import jpabook2.jpashop2.domain.item.Item;
 import jpabook2.jpashop2.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,24 @@ public class ItemService {
 	@Transactional
 	public void saveItem(Item item){
 		itemRepository.save(item);
+	}
+
+	@Transactional
+	public void updateItem( Long itemId ,  String name ,int price , int stockQuantity){
+
+		Item findItem = itemRepository.findOne(itemId); // itemId로 DB에서 영속성 엔티티를 가져온 것
+
+		findItem.setPrice(price);
+		findItem.setName(name);
+		findItem.setStockQuantity(stockQuantity);
+
+
+		/**
+		 * 이후에 아무것도 저장할 필요가 없다.
+		 * ex ) itemRepository.save( findItem ) 이런거..
+		 * 왜냐면 이미 findItem이 영속성 엔티티이기 때문에
+		 * jpa에서 변경을 감지하여 다 관리하고 있고 바로 업데이트해줌
+		 */
 	}
 
 	public List<Item> findItems(){
