6d428c88c178b80054a96333d1cfa9b4387ffe26
dh0522 <gong4857@naver.com>
2024-06-09 14:32:18 +0900

    상품 등록

---
+++ src/main/java/jpabook2/jpashop2/controller/ItemController.java
@@ -0,0 +1,57 @@
+package jpabook2.jpashop2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jpabook2.jpashop2.domain.item.Book;
+import jpabook2.jpashop2.service.ItemService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+	private final ItemService itemService;
+
+
+	@GetMapping("/items/new")
+	public String createForm(Model model){
+
+		model.addAttribute("form",new BookForm());
+		return "items/createItemForm";
+	}
+	@PostMapping("/items/new")
+	public String create(BookForm form){
+
+		Book book = new Book();
+
+		/**
+		 * 실무에서는 setter 다 날리고 구현하기
+		 */
+		book.setName(form.getName());
+		book.setPrice(form.getPrice());
+		book.setStockQuantity(form.getStockQuantity());
+		book.setIsbn(form.getIsbn());
+		book.setAuthor(form.getAuthor());
+
+
+		itemService.saveItem(book);
+
+		return "redirect:/";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
